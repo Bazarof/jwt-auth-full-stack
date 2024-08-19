06dfe7ffb89af066a753db18eef59a885d3a8836
Juan Jose Baza <baza_1546@outlook.com>
2024-08-19 12:21:25 -0600

    changes

--- Auth-API/obj/Debug/net8.0/Auth-API.AssemblyInfo.cs
+++ Auth-API/obj/Debug/net8.0/Auth-API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Auth-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aee302c1536bca4cbca9bfb5e1d5298d9034e7b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+988161cf04dd7d665403c6d4ce6b370fb4f7b87e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Auth-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Auth-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
